8bf891af08587f6f88c5efaa390836fe5dd86dd4
Rapp Anna <anjarapp18@gmail.com>
2023-06-15 22:39:06 +0700

    search page, search users

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -3,35 +3,27 @@ import UsersContainer from "../details/usersContainer";
 import MyTracks from "../profile/myTracks";
 
 const Search = (props) => {
+  console.log(props);
   return (
     <div style={{ marginLeft: "20px", fontSize: "20px" }}>
-      <p style={{ textAlign: "start" }}>Результаты поиска по "aaa"</p>
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: "purple",
+      <p style={{ textAlign: "start" }}>Результаты поиска по "{props.searchString}"</p>
+
+      <Tabs
+        tabPosition="left"
+        type="card"
+        items={[
+          {
+            label: <b>Пользователи</b>,
+            key: "1",
+            children: <UsersContainer users={props.users}></UsersContainer>,
           },
-        }}
-      >
-        <Tabs
-          tabPosition="left"
-          type="card"
-          
-          items={[
-            {
-              label: "Пользователи",
-              key: "1",
-              children: <UsersContainer></UsersContainer>,
-            },
-            {
-              label: "Песни",
-              key: "2",
-              children: <MyTracks></MyTracks>,
-            },
-            
-          ]}
-        />
-      </ConfigProvider>
+          {
+            label: <b>Песни</b>,
+            key: "2",
+            children: <MyTracks></MyTracks>,
+          },
+        ]}
+      />
     </div>
   );
 };
