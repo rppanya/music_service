e41a59b0cc81272aece07d276ba05da32a26070e
Rapp Anna <anjarapp18@gmail.com>
2023-06-20 00:46:41 +0700

    added search songs

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -6,7 +6,9 @@ const Search = (props) => {
   console.log(props);
   return (
     <div style={{ marginLeft: "20px", fontSize: "20px" }}>
-      <p style={{ textAlign: "start" }}>Результаты поиска по "{props.searchString}"</p>
+      <p style={{ textAlign: "start" }}>
+        Результаты поиска по "{props.searchString}"
+      </p>
 
       <Tabs
         tabPosition="left"
@@ -20,7 +22,12 @@ const Search = (props) => {
           {
             label: <b>Песни</b>,
             key: "2",
-            children: <MyTracks></MyTracks>,
+            children: <MyTracks songs={props.songs}></MyTracks>,
+          },
+          {
+            style: { display: "none" },
+            label: <b style={{ display: "none" }}>Песни</b>,
+            key: <div style={{ display: "none" }}></div>,
           },
         ]}
       />
--- src/components/search/searchContainer.js
+++ src/components/search/searchContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 
 import { connect } from "react-redux";
 import { searchUsersThunkCreator } from "../../store/reducers/userReducer";
+import { songsSearchThunkCreator } from "../../store/reducers/songsReducer";
 import Search from "./search";
 import { useLocation } from "react-router-dom";
 
@@ -10,6 +11,9 @@ function MiddleSearchContainer(props) {
 
   useEffect(() => {
     props.searchUsersThunkCreator(
+      new URLSearchParams(query).get("searchString")
+    );
+    props.songsSearchThunkCreator(
       new URLSearchParams(query).get("searchString")
     );
   }, [query]);
@@ -22,11 +26,12 @@ function MiddleSearchContainer(props) {
 }
 
 function mapStateToProps(state) {
-  return { users: state.user.usersSearch };
+  return { users: state.user.usersSearch, songs: state.songs.songsSearch };
 }
 
-const SearchContainer = connect(mapStateToProps, { searchUsersThunkCreator })(
-  MiddleSearchContainer
-);
+const SearchContainer = connect(mapStateToProps, {
+  searchUsersThunkCreator,
+  songsSearchThunkCreator,
+})(MiddleSearchContainer);
 
 export default SearchContainer;
