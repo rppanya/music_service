3b95b05122ed9a15f50a23ffea2b26806325cb2f
Rapp Anna <anjarapp18@gmail.com>
2023-06-20 00:47:57 +0700

    liked songs container

---
+++ src/components/details/likesContainer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import { connect } from "react-redux";
+
+import MyTracks from "../profile/myTracks";
+import { getLikedSongsThunkCreator } from "../../store/reducers/songsReducer";
+
+function MiddleLikesContainer(props) {
+  useEffect(() => {
+    props.getLikedSongsThunkCreator(props.userId);
+  }, [props.userId]);
+  console.log(props)
+  return <MyTracks {...props} />;
+}
+
+function mapStateToProps(state) {
+  return {
+    songs: state.songs.likedSongs,
+    currentPlaying: state.songs.currentPlaying,
+  };
+}
+
+const LikesContainer = connect(mapStateToProps, {
+    getLikedSongsThunkCreator,
+})(MiddleLikesContainer);
+
+export default LikesContainer;
